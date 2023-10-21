9654d11487c0c7aa99ef6c15548d11d47e9a0e35
Micha Buchholz <micha@haus23.net>
2023-10-21 22:56:48 +0200

    feat: Add reusable icon component.

---
+++ app/components/icon.tsx
@@ -0,0 +1,22 @@
+import type { SVGProps } from 'react';
+
+import { tw } from '#app/utils/misc';
+
+import iconFile from './icons.svg';
+
+const icons = ['avatar', 'close', 'computer', 'menu', 'moon', 'sun'] as const;
+export type IconName = (typeof icons)[number];
+
+export function Icon({
+  name,
+  className,
+  ...props
+}: SVGProps<SVGSVGElement> & {
+  name: IconName;
+}) {
+  return (
+    <svg {...props} className={tw('inline h-6 w-6 self-center', className)}>
+      <use href={`${iconFile}#${name}`} />
+    </svg>
+  );
+}
