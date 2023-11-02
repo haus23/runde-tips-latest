95cdc1e2f6d5d98b5d4afcefa91c867d7ea9a561
Micha Buchholz <micha@haus23.net>
2023-11-02 11:48:28 +0100

    refactor: Clean up. Remove all auth related code.

--- app/routes/_foh+/_auth+/login.tsx
+++ app/routes/_foh+/_auth+/login.tsx
@@ -1,61 +1,7 @@
-import type { DataFunctionArgs } from '@remix-run/node';
-import { useFetcher } from '@remix-run/react';
-
-import {
-  authenticate,
-  redirectIfAuthenticated,
-} from '#app/modules/auth/auth.server';
-
-export async function loader({ request }: DataFunctionArgs) {
-  await redirectIfAuthenticated(request);
-
-  return null;
-}
-
-export async function action({ request }: DataFunctionArgs) {
-  return await authenticate(request);
-}
-
 export default function LoginRoute() {
-  const authSubmission = useFetcher<typeof action>();
-
   return (
     <div>
       <h2>Anmeldung</h2>
-      <div>
-        <authSubmission.Form method="POST">
-          {(!authSubmission.data ||
-            authSubmission.data.status === 'AWAIT_EMAIL') && (
-            <input
-              type="email"
-              name="email"
-              autoComplete="email"
-              required
-              placeholder="Bekannte Email-Adresse"
-            ></input>
-          )}
-          {authSubmission.data &&
-            authSubmission.data.status === 'AWAIT_TOTP' && (
-              <input
-                type="text"
-                name="totp"
-                autoComplete="one-time-code"
-                required
-                placeholder="Dein Login-Code"
-              ></input>
-            )}
-        </authSubmission.Form>
-        {authSubmission.data && authSubmission.data.error && (
-          <span>{authSubmission.data.error}</span>
-        )}
-      </div>
-      {authSubmission.state !== 'idle' && (
-        <div>
-          {authSubmission.data?.status === 'AWAIT_TOTP'
-            ? 'Code wird überprüft'
-            : 'Email wird geprüft'}
-        </div>
-      )}
     </div>
   );
 }
