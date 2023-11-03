2c516b915988d1dfee609eb4ddc65e9daf25d466
Micha Buchholz <micha@haus23.net>
2023-11-03 13:25:34 +0100

    refactor: Drop valibot dependency for now. And simplify assertions.

---
+++ app/utils/route-handle.ts
@@ -0,0 +1,17 @@
+import { useMatches } from '@remix-run/react';
+
+type RouteHandle = {
+  viewPath: string;
+};
+
+function isRouteHandle(handle: unknown): handle is RouteHandle {
+  return typeof handle === 'object' && handle !== null && 'viewPath' in handle;
+}
+
+export function useViewSegment() {
+  const matches = useMatches();
+
+  // Get the viewPath-handle from the route leaf node
+  const handle = matches.at(-1)?.handle;
+  return isRouteHandle(handle) ? handle.viewPath : '';
+}
--- db/model.ts
+++
@@ -1,9 +0,0 @@
-import { createSelectSchema } from 'drizzle-valibot';
-import type { Output } from 'valibot';
-
-import { championshipTable, userTable } from './schema';
-
-export const Championship = createSelectSchema(championshipTable);
-
-export const User = createSelectSchema(userTable);
-export type User = Output<typeof User>;
