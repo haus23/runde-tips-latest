a603d3ddbc6a8fa8ce42c91953f75df3991f6c85
Micha Buchholz <micha@haus23.net>
2023-11-16 14:35:49 +0100

    feat: Add user menu.

--- app/components/user-menu.tsx
+++ app/components/user-menu.tsx
@@ -1,14 +1,41 @@
-import { Link } from '@remix-run/react';
+import {
+  Menu,
+  MenuItem,
+  MenuTrigger,
+  OverlayArrow,
+  Popover,
+} from 'react-aria-components';
 
 import { useUser } from '#app/utils/auth/user';
+
+import { Button } from './(ui)/button';
+import { Icon } from './icon';
 
 export function UserMenu() {
   const user = useUser();
 
   return (
-    <div className="flex gap-x-2">
-      {user.roles.includes('ADMIN') && <Link to="/manager">Manager</Link>}
-      <Link to="/logout">Log out</Link>
-    </div>
+    <MenuTrigger>
+      <Button aria-label="User Menu">
+        <Icon name="avatar" />
+      </Button>
+      <Popover placement="bottom end" className="rounded border bg-white">
+        <OverlayArrow>
+          <svg width={12} height={12} viewBox="0 0 12 12">
+            <path d="M0 12 L6 6 L12 12" />
+          </svg>
+        </OverlayArrow>
+        <Menu className="flex flex-col gap-y-2 px-4 py-2">
+          {user.roles.includes('ADMIN') && (
+            <MenuItem className="px-2 py-1" href="/manager">
+              Manager
+            </MenuItem>
+          )}
+          <MenuItem className="px-2 py-1" href="/logout">
+            Log out
+          </MenuItem>
+        </Menu>
+      </Popover>
+    </MenuTrigger>
   );
 }
