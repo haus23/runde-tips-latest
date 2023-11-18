74bfca239f2d08d66da7b7658fc7efd740de4319
Micha Buchholz <micha@haus23.net>
2023-11-18 12:53:11 +0100

    fix: Add press events to NavLink as well.

--- app/components/(ui)/atoms/link.tsx
+++ app/components/(ui)/atoms/link.tsx
@@ -68,10 +68,35 @@ Link.displayName = 'Link';
 
 interface NavLinkProps
   extends RemixNavLinkProps,
+    PressEvents,
     VariantProps<typeof linkVariants> {}
 
 const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
-  ({ className, variant, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      onPress,
+      onPressStart,
+      onPressEnd,
+      onPressChange,
+      onPressUp,
+      ...props
+    },
+    ref,
+  ) => {
+    const { pressProps } = usePress({
+      onPress,
+      onPressStart,
+      onPressEnd,
+      onPressChange,
+      onPressUp,
+    });
+
+    props = mergeProps(pressProps, {
+      ...props,
+    });
+
     return (
       <RemixNavLink
         className={linkVariants({ variant, className })}
